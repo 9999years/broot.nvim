24e7fa1e248042acffb73a889075ad1c77390b0f
Rebecca Turner <rbt@sent.as>
2023-10-08 15:08:53 -0700

    add test framework

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  description = "Broot integration for neovim";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    mini-nvim = {
+      url = "github:echasnovski/mini.nvim";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    mini-nvim,
+  }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ] (system:
+        function (import nixpkgs {
+          inherit system;
+        }));
+  in {
+    checks = forAllSystems (pkgs: let
+      commonArgs = {
+        src = ./.;
+
+        dontConfigure = true;
+        dontBuild = true;
+        doCheck = true;
+
+        installPhase = ''
+          touch $out
+        '';
+      };
+    in {
+      tests = pkgs.stdenv.mkDerivation (commonArgs
+        // {
+          name = "broot.nvim-tests";
+
+          nativeBuildInputs = [
+            pkgs.neovim
+            pkgs.broot
+          ];
+
+          MINI_NVIM = "${mini-nvim}";
+
+          checkPhase = ''
+            make test
+          '';
+        });
+
+      stylua = pkgs.stdenv.mkDerivation (commonArgs
+        // {
+          name = "broot.nvim-stylua";
+
+          nativeBuildInputs = [
+            pkgs.stylua
+          ];
+
+          checkPhase = ''
+            stylua --check .
+          '';
+        });
+
+      alejandra = pkgs.stdenv.mkDerivation (commonArgs
+        // {
+          name = "broot.nvim-alejandra";
+
+          nativeBuildInputs = [
+            pkgs.alejandra
+          ];
+
+          checkPhase = ''
+            alejandra --check .
+          '';
+        });
+    });
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        MINI_NVIM = "${mini-nvim}";
+
+        inputsFrom = builtins.attrValues self.checks.${pkgs.system};
+      };
+    });
+  };
+}
---
+++ tests/test_broot.lua
@@ -0,0 +1,53 @@
+local MiniTest = require("mini.test")
+local new_set = MiniTest.new_set
+local expect, eq = MiniTest.expect, MiniTest.expect.equality
+
+local child = MiniTest.new_child_neovim()
+
+local T = new_set {
+  hooks = {
+    pre_case = function()
+      child.restart { "-u", "scripts/mini_test_init.lua" }
+      child.lua([[
+        M = require("broot")
+        M.setup {
+          config_files = {
+            vim.fn.fnamemodify("tests/data/conf.toml", ":p"),
+            vim.fn.fnamemodify("tests/data/nvim.toml", ":p"),
+          },
+        }
+        vim.cmd("cd tests/data")
+      ]])
+    end,
+    post_once = child.stop,
+  },
+}
+
+local broot_test = new_set()
+T["broot"] = broot_test
+
+broot_test["can_open_file"] = function()
+  eq(child.lua_get([[M.broot()]]), vim.NIL)
+  vim.loop.sleep(250)
+  child.type_keys(100, "sam", "<CR>")
+  vim.loop.sleep(250)
+  expect.reference_screenshot(child.get_screenshot())
+end
+
+broot_test["can_use_directory"] = function()
+  eq(child.lua_get([[M.broot{directory = "flavors"}]]), vim.NIL)
+  vim.loop.sleep(250)
+  child.type_keys(100, "gol", "<CR>")
+  vim.loop.sleep(250)
+  expect.reference_screenshot(child.get_screenshot())
+end
+
+broot_test["can_use_extra_args"] = function()
+  eq(child.lua_get([[M.broot{extra_args = "--cmd c/sam"}]]), vim.NIL)
+  vim.loop.sleep(250)
+  child.type_keys("<CR>")
+  vim.loop.sleep(250)
+  expect.reference_screenshot(child.get_screenshot())
+end
+
+return T
