0c3d57711af266856fca719ce0b6b2e34d338b84
Rebecca Turner <rbt@sent.as>
2023-10-13 21:13:55 -0700

    Use `default_directory` function before switching buffers
    This fixes a bug which made the `config.default_directory`
    value not very useful.

--- tests/test_default_directory.lua
+++ tests/test_default_directory.lua
@@ -1,5 +1,5 @@
 local MiniTest = require "mini.test"
-local eq = MiniTest.expect.equality
+local expect, eq = MiniTest.expect, MiniTest.expect.equality
 
 local T, child = require("test").new_set { module = "broot.default_directory" }
 
@@ -13,6 +13,18 @@ end
 
 T["current_file"] = function()
   eq(child.lua_get [[M.current_file()]], vim.fn.expand "%:h")
+end
+
+T["current_file_2"] = function()
+  child.lua [[
+    require("broot").setup {
+      default_directory = require("broot.default_directory").current_file
+    }
+  ]]
+  child.cmd [[edit flavors/weenie]]
+  eq(child.lua_get [[require("broot").broot()]], vim.NIL)
+  vim.loop.sleep(250)
+  expect.reference_screenshot(child.get_screenshot())
 end
 
 return T
